d85a2c64a4cb68198683d3d354ddbd0bca9e4fe9
Suyan Man Amatya <srkamatya007@gmail.com>
2024-06-23 00:53:18 +0545

    Feature: JOD error Alert from backend

--- Server/router/auth-router.js
+++ Server/router/auth-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authControllers = require("../controllers/auth-controller");
-const signupSchema = require("../validators/auth-validator");
+const { signupSchema, loginSchema } = require("../validators/auth-validator");
 const validate = require("../middlewares/validate-middleware");
 const authMiddleware = require("../middlewares/auth-middleware.js");
 
@@ -10,7 +10,7 @@ router.route("/").get(authControllers.home);
 router
   .route("/register")
   .post(validate(signupSchema), authControllers.register);
-router.route("/login").post(authControllers.login);
+router.route("/login").post(validate(loginSchema), authControllers.login);
 router.route("/user").get(authMiddleware, authControllers.user);
 
 module.exports = router;
--- Server/validators/auth-validator.js
+++ Server/validators/auth-validator.js
@@ -1,23 +1,27 @@
 const { z } = require("zod");
 
-const signupSchema = z.object({
-  username: z
-    .string({ required_error: "Name is Required" })
-    .trim()
-    .min(3, { message: "Name atleast of 3 characters" }),
+const loginSchema = z.object({
   email: z
     .string({ required_error: "Email is Required" })
     .trim()
     .email({ message: "Invalid email Address" })
     .min(3, { message: "Email atleast of 3 characters" })
     .max(255, { message: "Email not more than 255 characters" }),
-  phone: z
-    .string({ required_error: "Phone is Required" })
-    .trim()
-    .min(3, { message: "Phone atleast of 3 characters" }),
   password: z
     .string({ required_error: "Password is Required" })
     .min(5, { message: "Password atleast of 4 characters" }),
 });
 
-module.exports = signupSchema;
+const signupSchema = loginSchema.extend({
+  username: z
+    .string({ required_error: "Name is Required" })
+    .trim()
+    .min(3, { message: "Name atleast of 3 characters" }),
+
+  phone: z
+    .string({ required_error: "Phone is Required" })
+    .trim()
+    .min(3, { message: "Phone atleast of 3 characters" }),
+});
+
+module.exports = { signupSchema, loginSchema };
